f4db5f2cd5cda201d325a47ec76bd72a984c253b
Wolfax <61865323+FrancoNicolaViggianoURJC@users.noreply.github.com>
2024-12-12 00:20:42 +0100

    Update procesar_numeros.R

--- procesar_numeros.R
+++ procesar_numeros.R
@@ -1,13 +1,43 @@
-leerArchivo <- function(nombreArchivo) { # create a function with the name my_function
+
+#Lectura fichero entrada
+leer_numeros <- function(nombreArchivo) {
   if(file.exists(nombreArchivo)){
     numeros <- scan(nombreArchivo, what = numeric())
     return(numeros)
   }else{
-    print("Error reading file. Make sure its is stored in the same directory as the script.")
+    print("Error al leer el archivo. Se debe encontrar en la carpeta Documentos.")
     stop()
   }
 }
+fichEntrada <- "./numeros.txt"
+numeros <-leer_numeros(fichEntrada)
 
-getwd()
-numeros <-leerArchivo("./numeros.txt")
+#Estadisticas
+media <- mean(numeros)
+mediana <- median(numeros)
+desviacionEstandar <- round( sd(numeros), digits=3)
 
+#Valores atipicos
+if(desviacionEstandar > 10){
+  print(paste("Hay alta variabilidad en los datos; SD de: ",  desviacionEstandar))
+}
+
+#Vector cuadrados
+elevarCuadrado <- function(x){
+  return(x*x)
+}
+
+cuadrados <- sapply(numeros, elevarCuadrado)
+
+#Escritura resultados
+  #Preprocesamiento
+  estadisticasToString <- c(
+    paste("Media:", media),
+    paste("Mediana:", mediana),
+    paste("Desviación estándar:", desviacionEstandar)
+  )
+  cuadradosTexto <- paste("Cuadrados:", cuadrados, collapse = "\n")
+  
+  archOut<-file("numerosSalida.txt")
+  writeLines(c(estadisticasToString, cuadradosTexto), archOut)
+  close(archOut)
