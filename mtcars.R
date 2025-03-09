c0cd8b6cf2dee36d967c0b5e0ed4b3bd1776d51b
Wolfax <61865323+FrancoNicolaViggianoURJC@users.noreply.github.com>
2025-03-09 23:09:07 +0100

    nottem

--- mtcars.R
+++ mtcars.R
@@ -1,41 +1,23 @@
 # Cargar librerías necesarias
 library(tseries)  # Para prueba ADF
-data("AirPassengers")
+data("nottem")
 
+# Graficar la serie temporal
+plot(nottem, main="Temperaturas Mensuales en Nottingham", 
+     xlab="Año", ylab="Temperatura", col="blue")
 
-# 2. Exploración inicial
-plot(AirPassengers, main="Serie Temporal de AirPassengers", ylab="Pasajeros", xlab="Año")
-
-# Cálculo de estadísticas descriptivas
-mean_value <- mean(AirPassengers)
-sd_value <- sd(AirPassengers)
-print(paste("Media:", mean_value))
-print(paste("Desviación Estándar:", sd_value))
-
-# 3. Análisis de tendencia y estacionalidad
-components <- decompose(AirPassengers)
+# 2. Exploración y preparación de datos
+components <- decompose(nottem)
 plot(components)
 
-# 4. Análisis de estacionariedad
-acf(AirPassengers, main="ACF de AirPassengers")
-pacf(AirPassengers, main="PACF de AirPassengers")
+acf(nottem, main="ACF de nottem")
+pacf(nottem, main="PACF de nottem")
 
-# Prueba de Dickey-Fuller
-adf_test <- adf.test(AirPassengers)
-print(adf_test)
+adf_test <- adf.test(nottem)
 
-# Si la serie no es estacionaria, diferenciamos
+# 4. Transformación de la serie (si es necesario)
 if (adf_test$p.value > 0.05) {
-  diff_series <- diff(AirPassengers)
+  diff_series <- diff(nottem)
   plot(diff_series, main="Serie Diferenciada", ylab="Diferencias", xlab="Año")
-  adf_test_diff <- adf.test(diff_series)
-  print(adf_test_diff)
 }
-
-# 5. Detección de valores atípicos
-boxplot(AirPassengers, main="Boxplot de AirPassengers", ylab="Pasajeros")
-
-# 6. Interpretación de resultados
-cat("\nResumen:\n")
-cat("La serie muestra una tendencia creciente, indicando un aumento en la cantidad de pasajeros a lo largo del tiempo.\n")
 
